24b6b0dd7902180b0f004ffd2d88e04c13b763c5
JerryLiang <zuly.gg@gmail.com>
2015-04-24 11:08:22 +0800

    voip conflict

--- voip/delete_user.php
+++ voip/delete_user.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by PhpStorm.
@@ -23,30 +22,4 @@ $voip->delete_data('T_phone_info',array('mobile'=>$mobile));
 $voip->delete_data('T_group_info',array('mobile'=>$mobile));
 $voip->delete_data('T_user_imei',array('mobile'=>$mobile));
 $ret = $voip->delete_data('T_user_info',array('username'=>$mobile));
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: jerry
- * Date: 2015/4/14 0014
- * Time: 11:20
- */
-define("BASEPATH", dirname(__FILE__) . '/');
-
-include_once BASEPATH . 'config/conf.php';
-include_once BASEPATH . 'model/voip.php';
-include_once INCLUDE_PATH . 'libraries/SmsGateway.class.php';
-include_once INCLUDE_PATH . 'libraries/Redis.class.php';
-
-$input = new CI_Input();
-$mobile = trim($input->get_post('mobile'));
-
-$voip = new voip();
-//删除im用户
-$voip->delete_im_user($mobile);
-$voip->delete_data('T_phone_info',array('mobile'=>$mobile));
-$voip->delete_data('T_group_info',array('mobile'=>$mobile));
-$voip->delete_data('T_user_imei',array('mobile'=>$mobile));
-$ret = $voip->delete_data('T_user_info',array('username'=>$mobile));
->>>>>>> 63b94a7e1b4167248a21999ae09c4dde81b786e9
 var_dump($ret);exit;
